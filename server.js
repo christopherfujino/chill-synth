b2cb8cfa7ca0b76dff31b66f06e0942d428c1575
Christopher Fujino <christopherfujino@gmail.com>
2020-02-08 16:03:10 -0800

    add a basic node server to get around CORS

---
+++ server.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  console.log(`Received a request for ${req.url}`);
+  fs.readFile(`.${req.url}`, function(err, content) {
+    if (err) {
+      console.log(`Error reading ${req.url} with ${err.code}`);
+      res.writeHead(500);
+      res.end("Oops!");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+app.listen(4000);
+console.log("listening on 4000");
