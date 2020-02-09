68b850c71b9347eb6fa7a9b95d91d9f64a3aad32
Christopher Fujino <christopherfujino@gmail.com>
2020-02-08 16:03:59 -0800

    create raw oscillator

--- main.js
+++ main.js
@@ -1,5 +1,64 @@
-const AudioContext = window.AudioContext || window.webkitAudioContext;
+class AudioPlayer {
+  constructor (
+    audioContext,
+    oscillator,
+    playButton,
+    volumeControl
+  ) {
+    this.audioContext = audioContext;
+    /** This is a VCO */
+    this.oscillator = oscillator;
+    this.playButton = playButton;
+    /** This is a VCA */
+    this.gainNode = audioContext.createGain();
+    this.oscillator.connect(this.gainNode)
+      .connect(audioContext.destination);
+    this.oscillator.type = "sine";
+    this.oscillator.frequency.value = 440;
+    this.oscillator.start(0);
+    this.volumeControl = volumeControl;
 
-const audioContext = new AudioContext();
-blah += doodoo;
-1 = 1 + 1;
+    this.init();
+  }
+
+  /** Set up callbacks */
+  init() {
+    const {
+      audioContext,
+      gainNode,
+      playButton,
+      volumeControl
+    } = this;
+    console.log("Initializing audio player...");
+
+    volumeControl.addEventListener("input", () => {
+      gainNode.gain.value = volumeControl.value;
+    });
+
+    playButton.addEventListener("click", function() {
+      console.log("clicked!");
+      // check if context is in suspended state (autoplay policy)
+      if (audioContext.state === "suspended") {
+        audioContext.resume();
+      } else if (audioContext.state === "running") {
+        audioContext.suspend();
+      }
+    });
+  }
+  /** AudioPlayer generator that creates dependencies and passes them to the
+    * constructor */
+  static createAudioPlayer() {
+    const audioContext = new AudioContext(),
+      oscillator = audioContext.createOscillator();
+
+    return new AudioPlayer(
+      audioContext,
+      oscillator,
+      document.querySelector("button"),
+      document.querySelector("#volume")
+    );
+  }
+}
+
+const audioPlayer = AudioPlayer.createAudioPlayer();
+
