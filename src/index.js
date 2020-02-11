709b75752a40e08e4b869855f974833eb96f1da0
Christopher Fujino <christopherfujino@gmail.com>
2020-02-10 20:56:02 -0800

    add another synth

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import Note from "./note.js";
 console.log("About to play a note...");
 
 const playButton = document.querySelector("#play-button");
-const rootNote = Note.create(48); // C3
-const chord = Chord.minorChord(rootNote);
 
 const synthOptions = {
   "oscillator": {
@@ -19,14 +17,36 @@ const synthOptions = {
     "release": 0.75,
   },
 };
-const synth = new Tone.Synth(synthOptions).toMaster();
+
+const synthOptions2 = {
+  "oscillator": {
+    "type": "square",
+  },
+  "envelope": {
+    "attack": 0.3,
+    "decay": 0.175,
+    "sustain": 0.7,
+    "release": 0.1,
+  },
+};
+const arpeggio = new Tone.Synth(synthOptions).toMaster();
+const bass = new Tone.Synth(synthOptions2).toMaster();
+
+const rootNote = Note.create(48); // C3
+const chord = Chord.minorChord(rootNote);
 
 const loop = new Tone.Loop((time) => {
   const note = chord.takeRandom();
   console.log(note.toString());
-  synth.triggerAttackRelease(note.toString(), "8n", time);
+  arpeggio.triggerAttackRelease(note.toString(), "8n", time);
 }, "4n");
+
+const bassNote = Note.create(36); // C2
+const bassLoop = new Tone.Loop((time) => {
+  bass.triggerAttackRelease(bassNote.toString(), "4n", time);
+}, "2n");
 loop.start(0);
+bassLoop.start(0);
 
 let isPlaying = false;
 playButton.addEventListener("click", () => {
