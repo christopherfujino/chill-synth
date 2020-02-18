f8ba9d089ec8b2630026bcf5b3cf7fe2dc2a2d12
Christopher Fujino <christopherfujino@gmail.com>
2020-02-17 18:04:28 -0800

    add dependency check

---
+++ scripts/check-dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+script_dir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+filename="$script_dir/../package-lock.json"
+
+# collect both times in seconds-since-the-epoch
+ten_days_ago=$(date -d 'now - 10 days' +%s)
+file_time=$(date -r "$filename" +%s)
+
+echo "$filename was last edited $(date -r "$filename")"
+if (( file_time <= ten_days_ago )); then
+  echo "$filename is older than 10 days"
+  exit 1
+else
+  echo "$filename is fresh."
+  exit 0
+fi
